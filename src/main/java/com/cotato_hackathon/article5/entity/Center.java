2be493281edaf22c8e5c52343932d3527487337b
seungmio <68382824+seungmio@users.noreply.github.com>
2023-06-30 23:09:00 +0900

    test: git push test

--- src/main/java/com/cotato_hackathon/article5/entity/Center.java
+++ src/main/java/com/cotato_hackathon/article5/entity/Center.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Center {
+public class Center { //
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
