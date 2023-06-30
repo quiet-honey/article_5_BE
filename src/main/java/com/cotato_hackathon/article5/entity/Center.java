4cc81b1ef6081021419207a91d6476a6399387aa
lildev1-admin <skap0329@naver.com>
2023-06-30 22:22:21 +0900

    add: Center Entity 추가

---
+++ src/main/java/com/cotato_hackathon/article5/entity/Center.java
@@ -0,0 +1,34 @@
+package com.cotato_hackathon.article5.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Center {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long centerId;
+
+    @Column(nullable = false)
+    private String centerName;
+
+    @Column(nullable = false)
+    private String district;
+
+    @Column(nullable = false)
+    private String neighbor;
+
+    private String detail;
+
+    @OneToMany(mappedBy = "center", cascade = CascadeType.ALL)
+    List<Meeting> meetingList;
+}
