0960ff134b2a50fd982591dafaa86cda35f0cc70
seungmio <68382824+seungmio@users.noreply.github.com>
2023-06-30 18:06:31 +0900

    feat: Initialize project

---
+++ src/main/java/com/cotato_hackathon/article5/Article5Application.java
@@ -0,0 +1,13 @@
+package com.cotato_hackathon.article5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Article5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Article5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotato_hackathon/article5/Article5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotato_hackathon.article5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Article5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
